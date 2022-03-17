38eaa2b6db87db4606f8a8aedc621c432c9b5943
tkyg <80114433+tkyg@users.noreply.github.com>
2022-03-17 17:41:10 -0400

    commit tree b222ad5a83f83eab34dd871d306f2623e3e130cc

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = new Set()
+  for (const number of array){
+    const complement = target - number
+    if (seenNumbers.has(complement)) return true
+    seenNumbers.add(number)
+  }
+  return false
 }
 
 /* 
@@ -8,6 +15,14 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  create an object to keep track of all the numbers we've seen 
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+  if so, return true
+  save the current number as the key on our object so we can check it later against other number
+  if we reach the end of the array, return false
 */
 
 /*
@@ -29,6 +44,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
 }
 
 module.exports = hasTargetSum;
